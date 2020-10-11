b9379934232d0fd99e39478d35f98775bbc3a61e
nkizito92 <nkizito57@gmail.com>
2020-10-10 23:41:23 -0500

    created a toggle for the links to hide or display

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,16 @@
 function hambugerBar() {
   let buns = document.querySelector(".buns");
   let links = document.getElementById("links");
+  let navBar = document.querySelector("nav");
+  links.hidden = true;
+
   buns.addEventListener("click", function () {
-    links.hidden === true ? links.hidden = false : links.hidden = true;
+    if (links.hidden === true) {
+      links.hidden = false;
+      navBar.style.height = "100%";
+    } else {
+      links.hidden = true;
+      navBar.style.height = "70px";
+    }
   });
 }
