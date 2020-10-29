5da3639178e40bc655cc8ab08e2a47c90ba138eb
nkizito92 <nkizito57@gmail.com>
2020-10-29 18:53:02 -0500

    new function added links to the sidebar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,30 @@
 function hambugerBar() {
   let buns = document.querySelector(".buns");
   let links = document.getElementById("links");
-  let navBar = document.querySelector("nav");
-  links.hidden = true;
+  let sideBar = document.querySelector("#addSideBar");
+  links.hidden= true;
+  let hideBar = true;
 
   buns.addEventListener("click", function () {
-    if (links.hidden === true) {
-      links.hidden = false;
-      navBar.className = "sidebar"
+    if (hideBar === true) {
+      // links.hidden = false;
+      hideBar = false;
+      appearingLinks();
+      sideBar.className = "sidebar";
     } else {
-      links.hidden = true;
-      navBar.className = "";
+      // links.hidden = true;
+      hideBar = true;
+      sideBar.className = "";
     }
   });
+
+  function appearingLinks() {
+    sideBar.innerHTML = 
+   `<div>
+    <li><a href="#portfolio"> Portfolio</a></li>
+    <li><a href="#about"> About Me </a></li>
+    <li><a href="#blog"> Blogs/Vlogs </a></li>
+    <li><a href="#contact"> Contacts</a></li>
+  </div>`
+  }
 }
